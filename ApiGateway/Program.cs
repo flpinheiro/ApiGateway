4bbadb35f8145cee585b2c8f20a9bc9f027d96fa
Felipe Pinheiro <felipe.pinheiro@compuletra.com.br>
2021-03-03 17:59:33 -0300

    funcionando

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,34 @@ namespace Compuletra.ApiGateway
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Verbose()
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.ForContext<Program>().Fatal(e, "fail");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((host, config) => 
             {
-                config.AddJsonFile("ocelot.json");
+                config
+                .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
             })
             .ConfigureWebHostDefaults(webBuilder =>
             {
